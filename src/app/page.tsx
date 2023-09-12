49916b89223cc75e81b1a08f9a548090883fdcff
KhallilB <khallilbailey@gmail.com>
2023-09-12 01:44:37 -0700

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { PageContainer, PageTitle } from '@/styles/global.style'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Courtside Empire</h1>
-    </div>
-  );
+    <PageContainer>
+      <div className="w-full flex justify-center items-center">
+        <PageTitle>Welcome to Courtside Empire!</PageTitle>
+      </div>
+    </PageContainer>
+  )
 }
