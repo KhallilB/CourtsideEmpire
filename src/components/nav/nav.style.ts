13a38fc07663f09d16cd7c8ce2217fa0551a6f57
KhallilB <khallilbailey@gmail.com>
2023-09-12 00:42:21 -0700

    update navbar

--- src/components/nav/nav.style.ts
+++ src/components/nav/nav.style.ts
@@ -1,14 +1,23 @@
-import tw from 'tailwind-styled-components';
+import tw from 'tailwind-styled-components'
 
 export const NavContainer = tw.nav`
-    w-screen
-    bg-jet
-    p-8
+  flex
+  w-screen
+  justify-center
+  bg-jet
+  p-8
 `
 export const LogoText = tw.h1`
-    text-white
-    text-2xl
-    font-bold
-    tracking-widest
-    uppercase
+  text-2xl
+  font-extrabold
+  uppercase
+  tracking-widest
+  text-white
+  px-4
+`
+export const NavContent = tw.div`
+  flex
+  max-w-screen-xl
+  justify-between
+  items-center
 `
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-
-import { NavContainer, LogoText } from "./nav.style";
+import { LogoText, NavContainer, NavContent } from './nav.style'
 
 const Nav = () => {
-    return (
-        <NavContainer>
-            <LogoText>Courtside Empire</LogoText>
-        </NavContainer>
-    )
+  return (
+    <NavContainer>
+      <NavContent>
+        <div className="flex flex-row">
+          <p className="px-4">About</p>
+          <p className="px-4">Leagues</p>
+          <p className="px-4">Offline</p>
+        </div>
+
+        <LogoText>Courtside Empire</LogoText>
+
+        <div className="flex flex-row">
+          <p className="px-4">Leaderboards</p>
+          <p className="px-4">Sign Up</p>
+          <p className="px-4">Log In</p>
+        </div>
+      </NavContent>
+    </NavContainer>
+  )
 }
 
-export default Nav;
+export default Nav
