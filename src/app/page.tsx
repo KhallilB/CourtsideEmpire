af1a70d52d6ab6b9216d9eec8941fed34238c428
KhallilB <khallilbailey@gmail.com>
2023-10-13 12:09:52 -0700

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { PageContainer, PageTitle } from '@/styles/global.style'
-
 export default function Home() {
-  return (
-    <PageContainer>
-      <div className="w-full flex justify-center items-center">
-        <PageTitle>Welcome to Courtside Empire!</PageTitle>
-      </div>
-    </PageContainer>
-  )
+    return (
+        <main>
+            <h1>Homeee</h1>
+        </main>
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,17 +5,14 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/styles/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    colors: {
-      black: '#050401',
-      jet: '#303036',
-      white: '#FFFAFF',
-      blue: '#007991',
-      red: '#D64933'
-    },
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
